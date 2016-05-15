145a018bc403550ec02c1fb43fbb9506da3a93dc
legendaryleo7 <legendaryleo7@hotmail.com>
2016-05-14 20:35:43 -0400

    Update gpio.py

--- gaugette/gpio.py
+++ gaugette/gpio.py
@@ -27,6 +27,20 @@ class GPIO:
             self.PUD_DOWN = self.gpio.PUD_DOWN
             self.PUD_OFF = self.gpio.PUD_OFF
             
+            elif gaugette.platform.isCHIP:
+            import CHIP_IO.GPIO
+            self.gpio = CHIP_IO.GPIO
+            self.setup = self.gpio.setup
+            self.output = self.gpio.output
+            self.input = self.gpio.input
+            self.OUT = self.gpio.OUT
+            self.IN = self.gpio.IN
+            self.HIGH = self.gpio.HIGH
+            self.LOW = self.gpio.LOW
+            self.PUD_UP = self.gpio.PUD_UP
+            self.PUD_DOWN = self.gpio.PUD_DOWN
+            self.PUD_OFF = self.gpio.PUD_OFF
+            
         elif gaugette.platform.isBeagleBoneBlack:
             import Adafruit_BBIO.GPIO
             self.gpio = Adafruit_BBIO.GPIO
